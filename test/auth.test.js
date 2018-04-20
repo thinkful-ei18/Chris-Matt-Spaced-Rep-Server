849e37f43d4a627a270ec6381676a0a89b222bbe
Chris Yang <cyang287@yahoo.com>
2018-04-20 10:01:44 -0400

    fixed auth and users tests

--- test/auth.test.js
+++ test/auth.test.js
@@ -1,109 +1,213 @@
 'use strict';
 
-require('dotenv').config();
-// global.TEST_DATABASE_URL = 'mongodb://localhost/jwt-auth-demo-test';
-const { User } = require('../models/user');
-const { JWT_SECRET, TEST_DATABASE_URL } = require('../config');
-
+const {app} = require('../index');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const jwt = require('jsonwebtoken');
-
-const {dbConnect, dbDisconnect} = require('../db-mongoose');
-const { app } = require('../index');
-
 const expect = chai.expect;
-
-// This let's us make HTTP requests
-// in our tests.
-// see: https://github.com/chaijs/chai-http
+const jwt = require('jsonwebtoken');
 
 chai.use(chaiHttp);
 
-describe('Auth endpoints', function () {
-  const fullname = 'exampleFullName';
+const mongoose = require('mongoose');
+const {TEST_DATABASE_URL, JWT_SECRET} = require('../config');
+const User = require('../models/user');
+
+describe('Auth endpoints', function() {
+  const fullname = 'Example User';
   const username = 'exampleUser';
   const password = 'examplePass';
-  const email = 'example@something.com';
-  // // console.log('hello');
-
+  const email = 'example@example.com';
+  
   before(function () {
-    // return runServer(TEST_DATABASE_URL);
-    return dbConnect(TEST_DATABASE_URL);    
+    return mongoose.connect(TEST_DATABASE_URL, {autoIndex: false});
   });
-
-  after(function () {
-    // return closeServer();
-    return dbDisconnect();  
+  
+  beforeEach(function() {
+    return User.hashPassword(password)
+      .then(password => {
+        User.create({
+          username,
+          password,
+          fullname,
+          email
+        });
+      });
   });
-
-  beforeEach(function () {
-    return User.hashPassword(password).then(password =>
-      User.create({
-        fullname,
-        username,
-        password,
-        email
-      })
-    );
-  });
-
-  afterEach(function () {
+  
+  afterEach(function() {
     return User.remove({});
   });
-
-  describe('/api', function () {
-    it('Should reject requests with no credentials', function () {
-      return chai
-        .request(app)
+  
+  after(function() {
+    return mongoose.disconnect();
+  });
+  
+  describe('POST api/auth/login', function() {
+    it('Should reject requests with no credentials', function() {
+      return chai.request(app)
         .post('/api/auth/login')
-        .then(() =>
-          expect.fail(null, null, 'Request should not succeed')
-        )
+        .then(() => {
+          expect.fail(null, null, 'Request should not succeed');
+        })
         .catch(err => {
           if (err instanceof chai.AssertionError) {
             throw err;
           }
-
+  
           const res = err.response;
           expect(res).to.have.status(400);
         });
     });
-
-    it('Should reject requests with incorrect usernames', function () {
-      return chai
-        .request(app)
+    it('Should reject incorrect usernames', function() {
+      return chai.request(app)
         .post('/api/auth/login')
-        .send({ username: 'wrongUsername', password })        
-        .then(() =>
-          expect.fail(null, null, 'Request should not succeed')
-        )
+        .send({username: 'wrongUsername', password})
+        .then(() => {
+          expect.fail(null, null, 'Request should not succeed');
+        })
         .catch(err => {
           if (err instanceof chai.AssertionError) {
             throw err;
           }
-
+  
           const res = err.response;
           expect(res).to.have.status(500);
         });
     });
-
-    it('Should reject requests with incorrect passwords', function () {
-      return chai
-        .request(app)
+    it('Should reject incorrect passwords', function() {
+      return chai.request(app)
         .post('/api/auth/login')
-        .send({ username, password: 'wrongPassword' })
-        .then(() =>
-          expect.fail(null, null, 'Request should not succeed')
-        )
+        .send({username, password: 'wrongPassword'})
+        .then(() => {
+          expect.fail(null, null, 'Request should not succeed');
+        })
         .catch(err => {
           if (err instanceof chai.AssertionError) {
             throw err;
           }
-
+  
           const res = err.response;
           expect(res).to.have.status(500);
         });
     });
-  })
+    it('Should return a valid auth token', function() {
+      return chai.request(app)
+        .post('/api/auth/login')
+        .send({username, password})
+        .then(res => {
+          console.log(res.users);
+          expect(res).to.have.status(200);
+          expect(res.body).to.be.an('object');
+          const token = res.body.authToken;
+          expect(token).to.be.a('string');
+          const payload = jwt.verify(token, JWT_SECRET, {algorithms: ['HS256']});
+          expect(payload.user.username).to.equal(username);
+          expect(payload.user.fullname).to.equal(fullname);
+        });
+    });
+  });
+  
+  describe('POST /api/auth/refresh', function() {
+    it('Should reject requests with no credentials', function() {
+      return chai.request(app)
+        .post('/api/auth/refresh')
+        .then(() => {
+          expect.fail(null, null, 'Request should not succeed');
+        })
+        .catch(err => {
+          if (err instanceof chai.AssertionError) {
+            throw err;
+          }
+  
+          const res = err.response;
+          expect(res).to.have.status(401);
+        });
+    });
+    it('Should reject requests with an invalid token', function() {
+      const token = jwt.sign(
+        {
+          username,
+          fullname
+        },
+        'wrongSecret',
+        {
+          algorithm: 'HS256',
+          expiresIn: '7d'
+        }
+      );
+  
+      return chai.request(app)
+        .post('/api/auth/refresh')
+        .set('Authorization', `Bearer ${token}`)
+        .then(() => {
+          expect.fail(null, null, 'Request should not succeed');
+        })
+        .catch(err => {
+          if (err instanceof chai.AssertionError) {
+            throw err;
+          }
+  
+          const res = err.response;
+          expect(res).to.have.status(401);
+        });
+    });
+    it('Should reject requests with an expired token', function() {
+      const token = jwt.sign(
+        {
+          user: {
+            username,
+            fullname
+          },
+          exp: Math.floor(Date.now() / 1000) -10
+        },
+        JWT_SECRET,
+        {
+          algorithm: 'HS256',
+          subject: username
+        }
+      );
+  
+      return chai.request(app)
+        .post('/api/auth/refresh')
+        .set('Authorization', `Bearer ${token}`)
+        .then(() => {
+          expect.fail(null, null, 'Request should not succeed');
+        })
+        .catch(err => {
+          if (err instanceof chai.AssertionError) {
+            throw err;
+          }
+  
+          const res = err.response;
+          expect(res).to.have.status(401);
+        });
+    });
+    it('Should return a valid auth token with a newer expiry date', function() {
+      const token = jwt.sign({
+        user: {
+          username,
+          fullname
+        }
+      },
+      JWT_SECRET,
+      {
+        algorithm: 'HS256',
+        subject: username,
+        expiresIn: '7d'
+      });
+  
+      return chai.request(app)
+        .post('/api/auth/refresh')
+        .set('Authorization', `Bearer ${token}`)
+        .then(res => {
+          expect(res).to.have.status(200);
+          expect(res.body).to.be.an('object');
+          const token = res.body.authToken;
+          expect(token).to.be.a('string');
+          const payload = jwt.verify(token, JWT_SECRET, {algorithm: ['HS256']});
+          expect(payload.user.username).to.equal(username);
+          expect(payload.user.fullname).to.equal(fullname);
+        });
+    });
+  });
 });
--- test/users.test.js
+++ test/users.test.js
@@ -1,175 +1,198 @@
 'use strict';
 
-require('dotenv').config();
-// global.TEST_DATABASE_URL = 'mongodb://localhost/jwt-auth-demo-test';
-const { User } = require('../models/user');
-const { JWT_SECRET, TEST_DATABASE_URL } = require('../config');
-
+const {app} = require('../index');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const jwt = require('jsonwebtoken');
-
-const {dbConnect, dbDisconnect} = require('../db-mongoose');
-const { app } = require('../index');
-
 const expect = chai.expect;
-
-// This let's us make HTTP requests
-// in our tests.
-// see: https://github.com/chaijs/chai-http
 
 chai.use(chaiHttp);
 
-describe('Auth endpoints', function () {
-  const fullname = 'exampleFullName';
+const mongoose = require('mongoose');
+const {TEST_DATABASE_URL} = require('../config');
+const User = require('../models/user');
+
+describe('User endpoints', function() {
   const username = 'exampleUser';
   const password = 'examplePass';
-  const email = 'example@something.com';
-
+  const fullname = 'Example User';
+  const email = 'example@example.com';
   const usernameB = 'exampleUserB';
   const passwordB = 'examplePassB';
-  const fullnameB = 'exampleFullNameB';
-  const emailB = 'example@something.comB';
-  
-  // // console.log('hello');
+  const fullnameB = 'Example UserB';
+
 
   before(function () {
-    // return runServer(TEST_DATABASE_URL);
-    return dbConnect(TEST_DATABASE_URL);    
+    return mongoose.connect(TEST_DATABASE_URL, {autoIndex: false});
   });
-
-  after(function () {
-    // return closeServer();
-    return dbDisconnect();  
+  
+  beforeEach(function() {
+    return User.hashPassword(password)
+      .then(password => {
+        User.create({
+          username,
+          password,
+          fullname,
+          email
+        });
+      });
   });
-
-  beforeEach(function () {
-    return User.hashPassword(password).then(password =>
-      User.create({
-        fullname,
-        username,
-        password,
-        email
-      })
-    );
-  });
-
-  afterEach(function () {
+  
+  afterEach(function() {
     return User.remove({});
   });
-
-  describe('/api/users', function() {
-    describe('POST', function() {
-      it('Should reject users with missing username', function() {
-        return chai
-          .request(app)
-          .post('/api/users')
-          .send({
-            password,
-          })
-          .then(() =>
-            expect.fail(null, null, 'Request should not succeed')
-          )
-          .catch(err => {
-            if (err instanceof chai.AssertionError) {
-              throw err;
-            }
-
-            const res = err.response;
-            expect(res).to.have.status(422);
-            expect(res.body.reason).to.equal('ValidationError');
-            expect(res.body.message).to.equal('Missing field');
-          });
-      });
-
-      it('Should reject users with missing password', function() {
-        return chai
-          .request(app)
-          .post('/api/users')
-          .send({
-            username,
-          })
-          .then(() =>
-            expect.fail(null, null, 'Request should not succeed')
-          )
-          .catch(err => {
-            if (err instanceof chai.AssertionError) {
-              throw err;
-            }
-
-            const res = err.response;
-            expect(res).to.have.status(422);
-            expect(res.body.reason).to.equal('ValidationError');
-            expect(res.body.message).to.equal('Missing field');
-          });
-      });
-
-      it('Should reject users with non-string username', function() {
-        return chai
-          .request(app)
-          .post('/api/users')
-          .send({
-            username: 1234,
-          })
-          .then(() =>
-            expect.fail(null, null, 'Request should not succeed')
-          )
-          .catch(err => {
-            if (err instanceof chai.AssertionError) {
-              throw err;
-            }
-
-            const res = err.response;
-            expect(res).to.have.status(422);
-            expect(res.body.reason).to.equal('ValidationError');
-          });
-      });
-
-      it('Should reject users with non-string password', function() {
-        return chai
-          .request(app)
-          .post('/api/users')
-          .send({
-            username,
-            password: 1234,
-          })
-          .then(() =>
-            expect.fail(null, null, 'Request should not succeed')
-          )
-          .catch(err => {
-            if (err instanceof chai.AssertionError) {
-              throw err;
-            }
-
-            const res = err.response;
-            expect(res).to.have.status(422);
-            expect(res.body.reason).to.equal('ValidationError');
-          });
-      });
-
-      it('Should reject users with empty username', function() {
-        return chai
-          .request(app)
-          .post('/api/users')
-          .send({
-            username: '',
-            password
-          })
-          .then(() =>
-            expect.fail(null, null, 'Request should not succeed')
-          )
-          .catch(err => {
-            if (err instanceof chai.AssertionError) {
-              throw err;
-            }
-
-            const res = err.response;
-            expect(res).to.have.status(422);
-            expect(res.body.reason).to.equal('ValidationError');
-          });
-      });
-    });
-
+  
+  after(function() {
+    return mongoose.disconnect();
   });
 
+  describe('POST /api/users', function() {
+    it('Should reject users with missing username', function() {
+      return chai.request(app)
+        .post('/api/users')
+        .send({
+          password,
+          fullname,
+          email
+        })
+        .then(() => {
+          expect.fail(null, null, 'Request should not succeed');
+        })
+        .catch(err => {
+          if (err instanceof chai.AssertionError) {
+            throw err;
+          }
+
+          const res = err.response;
+          expect(res).to.have.status(422);
+          expect(res.body.reason).to.equal('ValidationError');
+          expect(res.body.message).to.equal('Missing field');
+          expect(res.body.location).to.equal('hasFields');
+        });
+    });
+    it('Should reject users with missing password', function() {
+      return chai
+        .request(app)
+        .post('/api/users')
+        .send({
+          username,
+          fullname,
+          email
+        })
+        .then(() =>
+          expect.fail(null, null, 'Request should not succeed')
+        )
+        .catch(err => {
+          if (err instanceof chai.AssertionError) {
+            throw err;
+          }
+
+          const res = err.response;
+          expect(res).to.have.status(422);
+          expect(res.body.reason).to.equal('ValidationError');
+          expect(res.body.message).to.equal('Missing field');
+          expect(res.body.location).to.equal('hasFields');
+        });
+    });
+
+    it('Should reject users when a field is not a string', function() {
+      return chai
+        .request(app)
+        .post('/api/users')
+        .send({
+          username,
+          fullname,
+          email,
+          password: []
+        })
+        .then(() =>
+          expect.fail(null, null, 'Request should not succeed')
+        )
+        .catch(err => {
+          if (err instanceof chai.AssertionError) {
+            throw err;
+          }
+
+          const res = err.response;
+          expect(res).to.have.status(422);
+          expect(res.body.reason).to.equal('ValidationError');
+          expect(res.body.message).to.equal('Incorrect field type: expected string');
+          expect(res.body.location).to.equal('stringField');
+        });
+    });
+
+    it('Should reject users when a field starts or ends with a space', function() {
+      return chai
+        .request(app)
+        .post('/api/users')
+        .send({
+          username,
+          fullname,
+          email,
+          password: 'hello123 '
+        })
+        .then(() =>
+          expect.fail(null, null, 'Request should not succeed')
+        )
+        .catch(err => {
+          if (err instanceof chai.AssertionError) {
+            throw err;
+          }
+
+          const res = err.response;
+          expect(res).to.have.status(422);
+          expect(res.body.reason).to.equal('ValidationError');
+          expect(res.body.message).to.equal('Field cannot start or end with whitespace');
+          expect(res.body.location).to.equal('trimmedField');
+        });
+    });
+
+    it('Should reject users when password length is lower than 8', function() {
+      return chai
+        .request(app)
+        .post('/api/users')
+        .send({
+          username,
+          fullname,
+          email,
+          password: 'hello12'
+        })
+        .then(() =>
+          expect.fail(null, null, 'Request should not succeed')
+        )
+        .catch(err => {
+          if (err instanceof chai.AssertionError) {
+            throw err;
+          }
+
+          const res = err.response;
+          expect(res).to.have.status(422);
+          expect(res.body.reason).to.equal('ValidationError');
+          expect(res.body.message).to.equal('Username needs to be at least 1 character long and Password needs to be at least 8 characters long');
+          expect(res.body.location).to.equal('tooSmallField');
+        });
+    });
+
+    it('Should reject users when username already exists', function() {
+      return chai
+        .request(app)
+        .post('/api/users')
+        .send({
+          username,
+          fullname,
+          email,
+          password
+        })
+        .then(() =>
+          expect.fail(null, null, 'Request should not succeed')
+        )
+        .catch(err => {
+          if (err instanceof chai.AssertionError) {
+            throw err;
+          }
+          
+          const res = err.response;
+          expect(res).to.have.status(500);
+        });
+    });
+  });
 });
